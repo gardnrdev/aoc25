a77fd0d4c888fcd9c7856d150c1d9c01df561c6d
Jay Gardiner <jay@gardnr.dev>
2025-12-03 18:35:02 +1100

    part 2 done

--- day2/part2.rb
+++ day2/part2.rb
@@ -1,7 +1,7 @@
 invalid_ids = []
 final_sum   = 0
 
-input = File.read('./test_input').split(',')
+input = File.read('./input').split(',')
 
 input.each do |i|
     start_id, end_id = i.split('-').map(&:to_i)
@@ -9,19 +9,19 @@ input.each do |i|
     (start_id..end_id).each do |n|
         s = n.to_s
 
-        is_invalid = if s.length.even?
-            s[0...s.length/2] == s[s.length/2..-1]
-        elsif s.chars.uniq.length == 1
+        is_invalid = if s.chars.uniq.length == 1 && s.length > 1
             true
-        elsif s.length.odd?
-
+        elsif s.length.even? && s.length > 1
+            s[0...s.length/2] == s[s.length/2..-1] || (s[0..1] * (s.length / 2)) == s if s.length > 2
+        elsif s.length.odd? && s.length > 1
+            (s[0..2] * (s.length / 3)) == s if s.length > 3
         end
 
         if is_invalid
-            puts n
+            invalid_ids.push(n)
             final_sum += n
         end
     end
 end
 
-# puts final_sum
+puts final_sum
